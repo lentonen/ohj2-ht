6e6ace65b36c7b23d712748f05535123baed3862
Leinonen Henri Matias <henri.m.leinonen@student.jyu.fi>
2021-01-21 21:17:03 +0200

    GUI toimintoja

--- src/fxHuoltokirja/HuoltokirjaGUIController.java
+++ src/fxHuoltokirja/HuoltokirjaGUIController.java
@@ -1,10 +1,49 @@
 package fxHuoltokirja;
 
+import fi.jyu.mit.fxgui.Dialogs;
+import fi.jyu.mit.fxgui.ModalController;
+import javafx.fxml.FXML;
+
 /**
  * @author hemalein
  * @version Jan 15, 2021
  *
  */
 public class HuoltokirjaGUIController {
-	//
+   
+    @FXML void handleUusiPyora() {
+        uusiPyora();
+    }
+    
+    
+    @FXML void handlePoistaPyora() {
+        poistaPyora();
+    }
+    
+    
+    @FXML
+    void handleMuokkaaPyoraa() {
+        ModalController.showModal(HuoltokirjaGUIController.class.getResource("HuoltokirjaDialogGUIView.fxml"),
+                "Huoltokirja", null, "");
+    }
+    
+    
+    @FXML
+    void handleAvaaHuoltokirja(ActionEvent event) {
+        ModalController.showModal(HuoltokirjaGUIController.class.getResource("HuoltokirjaAukiGUIView.fxml"),
+                "Huoltokirja", null, "");
+    }
+    
+    
+    //=============================================================================================
+    
+    private void uusiPyora() {
+       Dialogs.showMessageDialog("Lisätään uusi pyörä. Ei osata lisätä vielä!");
+    }
+    
+    
+    private void poistaPyora() {
+        Dialogs.showQuestionDialog("Poisto?",
+                "Poistetaanko pyörä: Fuji Rakan", "Kyllä", "Ei");
+    }
 }
