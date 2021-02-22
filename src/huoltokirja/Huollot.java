e7af1715b53ba790202c9c4c2d2acca28d43dc5e
Leinonen Henri Matias <henri.m.leinonen@student.jyu.fi>
2021-02-22 13:56:10 +0200

    huolto ja huollot luokkien aloitus

--- src/huoltokirja/Huollot.java
+++ src/huoltokirja/Huollot.java
@@ -3,19 +3,124 @@
  */
 package huoltokirja;
 
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Iterator;
+import java.util.List;
+
 /**
  * @author Henri
  * @version Feb 11, 2021
  *
  */
-public class Huollot {
-
+public class Huollot implements Iterable<Huolto> {
+    private String tiedostonNimi ="";
+    
+    private final Collection<Huolto> huollot= new ArrayList<Huolto>();
+    
     /**
+     * oletusmuodostaja
+     */
+    public Huollot() {
+        //
+    }
+    
+    
+    /**
+     * Lisätään huolto valmiiseen tietorakenteeseen
+     * @param huolto lisättävä huolto
+     */
+    public void lisaa(Huolto huolto) {
+        huollot.add(huolto);
+    }
+    
+    
+    /**
+     * Lukee huoltojen tiedot tiedostosta. TODO: tee huoltojen luku loppuun
+     * @param hakemisto kertoo missä tiedosto sijaitsee
+     * @throws ApuException minkälainen virheilmoitus näytetään.
+     */
+    public void lueTiedosto(String hakemisto) throws ApuException {
+        tiedostonNimi = hakemisto + "huollot.dat";
+        throw new ApuException("Ei osata lukea vielä tiedostoa " +tiedostonNimi);
+    }
+    
+    
+    /**
+     * Tallettaa huoltojen tiedot tiedostoon huollot.dat TODO: tee huoltojen tallettaminen loppuun
+     * @throws ApuException minkälainen virheilmoitus näytetään
+     */
+    public void talleta() throws ApuException {
+        //tiedostonNimi = hakemisto + "pyorat.dat"; TODO: tarviiko tätä jossakin vaiheessa?
+        throw new ApuException("Ei osata tallentaa tiedostoon" +tiedostonNimi);
+    }
+    
+    
+    /**
+     * Palauttaa tallennettujen huoltojen lukumäärän
+     * @return huoltojen lukumäärä
+     */
+    public int getLkm() {
+        return huollot.size();
+    }
+    
+    
+    /**
+     * Palauttaa listan annetun pyörän huolloista. //TODO:testit
+     * @param pyoraNro Pyörä jonka huoltoja etsitään
+     * @return Viite listaan pyörän huolloista
+     * @throws IndexOutOfBoundsException virhe jos yritetään etsiä sellaisen pyörän huoltoa, jota ei ole lisätty
+     */
+    public List<Huolto> annaHuollot(int pyoraNro) throws IndexOutOfBoundsException {
+        List<Huolto> loydetyt = new ArrayList<Huolto>();
+        for (Huolto huolto: huollot)
+            if (huolto.getPyoraNro() == pyoraNro) loydetyt.add(huolto);
+        return loydetyt;
+    }
+    
+    
+    /**
+     * Testataan huollot-luokan toimintaa
      * @param args ei käytössä
      */
     public static void main(String[] args) {
-    // TODO Auto-generated method stub
+        Huollot huollot = new Huollot();
+        
+        // Arvotaan huoltoja pyörälle 1
+        Huolto huolto1 = new Huolto(1); huolto1.arvoHuolto(); huolto1.rekisteroi(); 
+        Huolto huolto2 = new Huolto(1); huolto2.arvoHuolto(); huolto2.rekisteroi(); 
+        
+        // Arvotaan huoltoja pyörälle 2
+        Huolto huolto3 = new Huolto(2); huolto3.arvoHuolto(); huolto3.rekisteroi();     
+        Huolto huolto4 = new Huolto(2); huolto4.arvoHuolto(); huolto4.rekisteroi(); 
+        Huolto huolto5 = new Huolto(2); huolto5.arvoHuolto(); huolto5.rekisteroi(); 
+        
+        // Lisätään arvotut huollot
+        huollot.lisaa(huolto1);
+        huollot.lisaa(huolto2);
+        huollot.lisaa(huolto3);
+        huollot.lisaa(huolto4);
+        huollot.lisaa(huolto5);
+        
+        // Tulostetaan pyörän 1 / 2 huollot
+        
+        List<Huolto> pyoranHuollot = huollot.annaHuollot(2);
+        for (Huolto huolto : pyoranHuollot) {
+            huolto.tulosta(System.out);
+        }
+        
+    }
+
     
+    
+    
+    
+    
+    
+    @Override
+    public Iterator<Huolto> iterator() {
+        // TODO Auto-generated method stub
+        return null;
     }
 
 }
--- src/huoltokirja/Huolto.java
+++ src/huoltokirja/Huolto.java
@@ -3,18 +3,146 @@
  */
 package huoltokirja;
 
+import static huoltokirja.Apulaskut.rand;
+
+import java.io.OutputStream;
+import java.io.PrintStream;
+
 /**
  * @author Henri
  * @version Feb 11, 2021
  *
  */
 public class Huolto {
+    private int tunnusNro;
+    private int pyoraNro;
+    private String nimi             = "";
+    private int ajotunnit;
+    private String toimenpiteet     = "";
+    
+    private static int seuraavaNro = 1;  // static = "tämä on olemassa, vaikka olioita ei olisi luotukaan." TODO:tarvitaanko missään?
+    
+    
+    /**
+     * Oletusmuodostaja
+     */
+    public Huolto() {
+    }
+    
+    
+    /**
+     * muodostaja jolle viedään pyörän numero.
+     * @param pyoraID pyörä, johon huolto halutaan liittää
+     */
+    public Huolto(int pyoraID) {
+        this.pyoraNro = pyoraID;
+    }
+    
+    
+    /**
+     * Palauttaa huollon nimen
+     * @return huollon nimi
+     * @example
+     * <pre name="test">
+     * Huolto huolto = new Huolto();
+     * huolto.getNimi() === "";
+     * </pre>
+     */
+    public String getNimi() {
+        return nimi;
+    }
+    
+    
+    /**
+     * Saantimetodi ajotunneille
+     * @return huollon ajotunnit
+     * @example
+     * <pre name="test">
+     * Huolto huolto = new Huolto();
+     * huolto.getAjotunnit() === 0;
+     * </pre>
+     */
+    public int getAjotunnit() {
+        return ajotunnit;
+    }
+    
+    
+    /**
+     * Tulostetaan huollon tiedot parametrina tuotuun tietovirtaan
+     * @param os tietovirta johon tiedot tulostetaan
+     */
+    public void tulosta(OutputStream os) {
+        PrintStream out = new PrintStream(os);              // OutputStream käytössä tulevaisuutta varten. Printstreamilla mm. prinln-metodi. 
+        out.println("HuoltoID: "+tunnusNro + "\n"        // OutputStreamin hankala käyttö ohitetaan luomalla printstream ja käyttämällä sitä tulostamiseen.
+                + "PyöräID: "+pyoraNro + "\n"
+                + "Nimi: "+nimi + "\n" 
+                +"Ajotunnit: "+ajotunnit + "\n" 
+                +"Malli: "+toimenpiteet + "\n");
+    }
+       
+    
+    /**
+     * Antaa huollolle seuraavan vapaana olevan tunnusNro:n.
+     * @return Vapaana oleva tunnusNro.
+     * @example
+     * <pre name="test">
+     *  Huolto huolto1 = new Huolto();
+     *  huolto1.rekisteroi();
+     *  huolto.getTunnusNro() === 1;
+     *  Huolto huolto2 = new Huolto();
+     *  huolto2.rekisteroi();
+     *  huolto2.getTunnusNro() === 2;
+     * </pre>
+     */
+    public int rekisteroi() {
+        tunnusNro = seuraavaNro;
+        seuraavaNro++;
+        return tunnusNro;
+    }
+    
+    
+    /**
+     * Palauttaa huollon tunnusnumeron.
+     * @return tunnusnumero
+     */
+    public int getTunnusNro() {
+        return tunnusNro;
+    }
+    
+    
+    /**
+     * Palauttaa huollon pyoraNro.
+     * @return pyörän numero
+     */
+    public int getPyoraNro() {
+        return pyoraNro;
+    }
+    
+    /**
+     * Arpoo mallin vuoksi dataan huoltoja eri tiedoilla
+     */
+    public void arvoHuolto() {
+        nimi         = "Huolto " + rand(1,9999);
+        ajotunnit    = rand(1,500);
+        toimenpiteet = "Vaihdettiin osanro " +rand(1,9999) + " ja putsattiin osa " +rand(1,200);
+    }
+    
+    
 
     /**
+     * Pääohjelma Huolto-olion testaamiseen.
      * @param args ei käytössä
      */
     public static void main(String[] args) {
-    // TODO Auto-generated method stub
+        Huolto huolto = new Huolto(1);
+        huolto.arvoHuolto();
+        huolto.rekisteroi();
+        huolto.tulosta(System.out);
+        
+        Huolto huolto2 = new Huolto(5);
+        huolto2.arvoHuolto();
+        huolto2.rekisteroi();
+        huolto2.tulosta(System.out);
     
     }
 
